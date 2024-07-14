6e5693ed12de0abf8764586318c7525352839ca6
rubenduarte2905 <rubenduartemarecos@gmail.com>
2024-07-14 11:15:29 -0300

    crea ficheros

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php 
-     echo "Hola Mundo";
-     echo "Hello World";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+</head>
+<body>
+     
+</body>
+</html>
